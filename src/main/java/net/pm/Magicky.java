6268c76b884bde7158689d6b02e39e22fb7fe675
PaulMarmet <paulmarmet1002@gmail.com>
2024-06-23 21:11:21 -0400

    Renamed mod to "Magicky" Added Magic Protection Fixed Projectile Prot not using the proper exclusion tag. Frost Walker now only works while equipped.

---
+++ src/main/java/net/pm/Magicky.java
@@ -0,0 +1,14 @@
+package net.pm;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Magicky implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("magicky");
+
+	@Override
+	public void onInitialize() {
+	}
+}
